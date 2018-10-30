bfe20505d1c5bca84f7bd61f9eeb998d4e734273
gibum1228 <gibum1228@naver.com>
2018-10-31 01:43:57 +0900

    print, input 활용
    2018.10.31.

---
+++ lab2.py
@@ -0,0 +1,24 @@
+#1
+print("김기범")
+
+#2
+name = "김기범"
+print(name)
+
+#3
+age = 20
+print(age)
+
+#4
+name = input("이름을 입력하시오: ")
+print("이름: %s" %name)
+
+#5
+age = input("나이를 입력하시오: ")
+print("나이: %s" %age)
+
+#6
+name = input("이름 입력: ")
+age = input("나이 입력: ")
+text = "나의 이름은 %s이고, 나이는 %s세 입니다."
+print(text %(name, age))
