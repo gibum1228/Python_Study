10cc26b23bb4288eee2163f0c74075dcfc90c5d4
gibum1228 <gibum1228@naver.com>
2018-11-01 00:40:23 +0900

    사칙연산하기
    2018.11.01.

---
+++ lab3.py
@@ -0,0 +1,14 @@
+#Day 3 연산자와 형변환 연습하기
+
+a = 15
+b = 4
+
+print("%d + %d = %d" %(a, b, a+b)) #덧셈
+print("%d - %d = %d" %(a, b, a-b)) #뺄셈
+print("%d * %d = %d" %(a, b, a*b)) #곱셈
+print("%d / %d = %d" %(a, b, a/b)) #나눗셈 정수
+print("%d // %d = %d" %(a, b, a//b)) #나눗셈 정수 몫만 보기
+print("%f / %f = %f" %(a, b, a/b)) #나눗셈 실수
+print("%f // %f = %f" %(a, b, a//b)) #나눗셈 실수 몫만 보기
+print("%d %% %d = %d" %(a, b, a%b)) #나머지
+print("%d ** %d = %d" %(a, b, a**b)) #승수
